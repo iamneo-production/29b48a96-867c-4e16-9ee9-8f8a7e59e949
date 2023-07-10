806796cf4f60d7680d81c81495c0db4bcb782a75
dshritama <dshritama@gmail.com>
2023-07-10 04:26:27 +0000

    full integrated backend

---
+++ springapp/src/main/java/com/example/springapp/controller/StaffController.java
@@ -0,0 +1,58 @@
+package com.example.springapp.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+
+import com.example.springapp.model.Staff;
+import com.example.springapp.service.StaffService;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffController {
+    private final StaffService staffService;
+
+    public StaffController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping
+    public List<Staff> getAllStaff() {
+        return staffService.getAllStaff();
+    }
+
+    @PostMapping
+    public Staff createStaff(@RequestBody Staff staff) {
+        return staffService.createStaff(staff);
+    }
+
+    @PutMapping
+    public Staff updateStaff(@RequestBody Staff staff) {
+        return staffService.updateStaff(staff);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStaff(@PathVariable Long id) {
+        staffService.deleteStaff(id);
+        return "Deleted";
+    }
+    @GetMapping("/doctor/{id}")
+    public ResponseEntity<Staff> getDoctorById(@PathVariable Long id) {
+        Staff doctor = staffService.getDoctorById(id);
+        if (doctor != null) {
+            return ResponseEntity.ok(doctor);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
