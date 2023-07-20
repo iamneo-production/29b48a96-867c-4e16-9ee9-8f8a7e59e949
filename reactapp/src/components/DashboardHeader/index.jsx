08fcc7f7278e581562d77846f718802631574c55
dshritama <dshritama@gmail.com>
2023-07-20 02:45:36 +0000

    code commited to (dshritama

---
+++ reactapp/src/components/DashboardHeader/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './styles.css';
+
+function DashboardHeader ({ btnText, onClick }) {
+    return(
+        <div className='dashbord-header-container'>
+        {btnText && 
+            <button className='dashbord-header-btn' onClick={onClick}>{btnText}</button>
+        }
+        
+        <div className='dashbord-header-right'>
+        <span>Welcome Admin</span>
+            {/* <img 
+                src={NotificationIcon}
+                alt='notification-icon'
+                className='dashbord-header-icon' />
+            <img 
+                src={SettingsIcon}
+                alt='settings-icon'
+                className='dashbord-header-icon' />
+            <img
+                className='dashbord-header-avatar'
+                src='https://reqres.in/img/faces/9-image.jpg' /> */}
+        </div>
+    </div>
+    )
+}
+
+export default DashboardHeader;
