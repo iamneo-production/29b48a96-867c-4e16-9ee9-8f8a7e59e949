5bf16f0d9c3d9dceec590c355d28414a4d2e22ee
Sriharsha0612 <karisriharsha1716@gmail.com>
2023-07-29 13:57:57 +0000

    test-cases commit

--- springapp/src/main/java/com/dshritama/springapp/repository/StaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dshritama.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dshritama.springapp.model.Staff;
-
-public interface StaffRepository extends JpaRepository<Staff, Long> {
-}
---
+++ springapp/src/main/java/com/example/springapp/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springapp.model.Inventory;
+
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long>{
+    
+}
